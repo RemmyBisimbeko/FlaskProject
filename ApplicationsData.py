1ba21e57bec20ecfd35ff50aeb45d23acad26fe8
Remmy Bisimbeko <bisremmy.rb@gmail.com>
2020-06-27 22:46:51 +0300

    Addes Application route, dope + modules

---
+++ ApplicationsData.py
@@ -0,0 +1,37 @@
+def Applications():
+    applications = [
+        {
+            'id': 1,
+            'name': 'Kenneth Kyobe',
+            'account': '3210008900',
+            'branch': 'Mapeera',
+            'telephone': '+256 701 234 567',
+            'email': 'kenneth.kyobe@mail.com',
+            'officer': 'Ndugga Jude',
+            'reason': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+            'submission_date': '31-05-2020'
+        },
+        {
+            'id': 2,
+            'name': 'Remmy Bisimbeko',
+            'account': '3210008992',
+            'branch': 'Mapeera Platinum',
+            'telephone': '+256 701 234 567',
+            'email': 'remmy.bisimbeko@mail.com',
+            'officer': 'Mukasa Martin',
+            'reason': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+            'submission_date': '01-06-2020'
+        },
+        {
+            'id': 3,
+            'name': 'Derick Kasirye',
+            'account': '3210788900',
+            'branch': 'Entebbe Road',
+            'telephone': '+256 721 234 567',
+            'email': 'derick.kasirye@mail.com',
+            'officer': 'Ssempijja Moses',
+            'reason': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+            'submission_date': '02-06-2020'
+        }
+    ]
+    return applications
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from data import Restructures
+# from ApplicationsData import Applications
 
 # Init App
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 Restructures = Restructures()
 
 # Create Variable equal to Function imported 
-Applications = Applications()
+# Applications = Applications()
 
 # Home Route
 @app.route('/')
@@ -20,27 +21,25 @@ def home():
 def about():
     return render_template('about.html')
 
-# Restrucres Route
+# Restructures Route
 @app.route('/restructures')
 def restructures():
     return render_template('restructures.html', restructures = Restructures)
 
-# Single Restrucre Route
+# Single Restructure Route
 @app.route('/restructure/<string:id>')
 def restructure(id):
     return render_template('restructure.html', id=id)
 
 # Applications Route
 @app.route('/applications')
-def restructures():
-    return render_template('applications.html', applications = Applications)
+def applications():
+    return render_template('applications.html', restructures = Restructures)
 
-# Single Restrucre Route
+# Single Application Route
 @app.route('/application/<string:id>')
-def restructure(id):
+def application(id):
     return render_template('application.html', id=id)
-
-
 
 # Run Server
 if __name__ == '__main__':
