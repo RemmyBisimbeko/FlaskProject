58f6ddf76832bb6392d50cae3073e3d1293f829b
Remmy Bisimbeko <bisremmy.rb@gmail.com>
2020-06-27 23:59:34 +0300

    user registration

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-from data import Restructures
-# from ApplicationsData import Applications
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from RestructuresData import Restructures
+from ApplicationsData import Applications
+from EnrollmentsData import Enrollments
+
+from flask_mysqldb import MySQL
+# Import wtforms and  Each Type of Field to be used
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+# Import passlib hash
+from passlib.hash import sha256_crypt
 
 # Init App
 app = Flask(__name__)
 
 # Create Variable equal to Function imported 
 Restructures = Restructures()
-
-# Create Variable equal to Function imported 
-# Applications = Applications()
+Applications = Applications()
+Enrollments = Enrollments()
 
 # Home Route
 @app.route('/')
@@ -20,6 +26,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register')
+def register():
+    form = RegisterForm(request.form)
+    # Check if GET or POST request, and make sure everything is validated
+    # if request.method == 'POST' and form.validate():
+        # return render_template('register.html')
+    return render_template('register.html', form=form)
 
 # Restructures Route
 @app.route('/restructures')
@@ -34,12 +58,22 @@ def restructure(id):
 # Applications Route
 @app.route('/applications')
 def applications():
-    return render_template('applications.html', restructures = Restructures)
+    return render_template('applications.html', applications = Applications)
 
 # Single Application Route
 @app.route('/application/<string:id>')
 def application(id):
     return render_template('application.html', id=id)
+
+# Enrollments Route
+@app.route('/enrollments')
+def enrollments():
+    return render_template('enrollments.html', enrollments = Enrollments)
+
+# Single Enrollment Route
+@app.route('/enrollment/<string:id>')
+def enrollment(id):
+    return render_template('enrollment.html', id=id)
 
 # Run Server
 if __name__ == '__main__':
